a28e7c7aa4a4d3ad14eb968745230a604940a290
Salman <salkhan7964@gmail.com>
2025-01-05 14:42:21 -0600

    renamed project to textSummarizer and added logging module to project

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-project_name = 'Text-Summarizer'
+project_name = 'textSummarizer'
 
 list_of_files = [
     '.github/workflows/.gitkeep',
