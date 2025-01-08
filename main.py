2c23a3f631a1dc1381be677dad331a33191b0b83
Salman <salkhan7964@gmail.com>
2025-01-08 15:55:53 -0600

    added logic for model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_3_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_4_model_training import ModelTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = 'Stage 1: Data Ingestion'
@@ -32,6 +33,17 @@ try:
     logger.info(f"Running {STAGE_NAME}")
     data_transformation_pipeline = DataTransformationTrainingPipeline()
     data_transformation_pipeline.main()
+    logger.info(f"{STAGE_NAME} completed successfully")
+except Exception as e:
+    logger.error(f"{STAGE_NAME} failed: {str(e)}")
+    raise e
+
+STAGE_NAME = 'Stage 4: Model Training'
+
+try:
+    logger.info(f"Running {STAGE_NAME}")
+    model_training_pipeline = ModelTrainingPipeline()
+    model_training_pipeline.main()
     logger.info(f"{STAGE_NAME} completed successfully")
 except Exception as e:
     logger.error(f"{STAGE_NAME} failed: {str(e)}")
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from src.textSummarizer.constant import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from src.textSummarizer.utils.common import read_yaml, create_directories
 from pathlib import Path
-from src.textSummarizer.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from src.textSummarizer.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 
 class ConfigurationManager:
     def __init__(self, 
@@ -12,6 +12,7 @@ class ConfigurationManager:
         
         create_directories([self.config.artifacts_root])
     
+    # data ingestion
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
         create_directories([config.root_dir])
@@ -22,7 +23,8 @@ class ConfigurationManager:
             local_data_dir = Path(config.local_data_dir),
             unzip_data_dir = Path(config.unzip_data_dir)
         )
-        
+     
+    # data validation    
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         create_directories([config.root_dir])
@@ -32,7 +34,8 @@ class ConfigurationManager:
             STATUS_FILE = config.STATUS_FILE,
             ALL_REQUIRED_FILES = config.ALL_REQUIRED_FILES
         )
-        
+     
+    # data transformation    
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
         
@@ -43,3 +46,27 @@ class ConfigurationManager:
             data_path = Path(config.data_path),
             tokenizer_name = config.tokenizer_name
         )
+        
+    # model training    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
+        
+        create_directories([config.root_dir])
+        
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            data_path = config.data_path,
+            model_ckpt = config.model_ckpt,
+            num_train_epochs = params.num_train_epochs,
+            warmup_steps = params.warmup_steps,
+            per_device_train_batch_size = params.per_device_train_batch_size,
+            weight_decay = params.weight_decay,
+            logging_steps = params.logging_steps,
+            evaluation_strategy = params.evaluation_strategy,
+            eval_steps = params.eval_steps,
+            save_steps = params.save_steps,
+            gradient_accumulation_steps = params.gradient_accumulation_steps
+        )
+
+        return model_trainer_config
